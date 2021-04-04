2978bc0e144c6aba7248bb69046f1933da0cbc0d
codeup1054 <codeup1054@gmail.com>
2021-04-04 16:05:43 +0300

    Minin canvas for telegram before compute min max

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-// 2021-04-04 https://youtu.be/yz09oVHt3PY?t=2098
+// 2021-04-04 https://youtu.be/yz09oVHt3PY?t=2803
 
 const  WIDTH  = 600
 const HEIGHT =  200
+const PADDING = 40
+
 const DPI_WIDTH = WIDTH*2
 const DPI_HEIGHT = HEIGHT*2
+const VIEW_HEIGHT = DPI_HEIGHT - PADDING*2
+const ROW_COUNT = 5
+
 
 
 function chart(canvas,data) {
@@ -13,13 +18,35 @@ function chart(canvas,data) {
     canvas.width = DPI_WIDTH
     canvas.height = DPI_HEIGHT
 
+
+    const step = VIEW_HEIGHT/ROW_COUNT
+
+    ctx.beginPath()
+
+    ctx.strokeStyle = "#bbb"
+    ctx.font = 'normal 20px Helvetica,sans-serif'
+    ctx.fillStyle = '#96a2aa'
+
+    for(let i= 1; i <= ROW_COUNT; i++)
+    {
+        const y = step*i
+
+        ctx.fillText(DPI_HEIGHT-y ,5,y-10 +PADDING)
+        ctx.moveTo(0,y+PADDING)
+        ctx.lineTo(DPI_WIDTH,y+PADDING)
+    }
+
+    ctx.stroke()
+
+    ctx.closePath()
+
     ctx.beginPath()
     ctx.lineWidth = 4
     ctx.strokeStyle = '#ff0000'
 
     for (const[x,y] of data)
     {
-        ctx.lineTo(x, DPI_HEIGHT -y)
+        ctx.lineTo(x, DPI_HEIGHT -y - PADDING)
     }
     ctx.stroke()
     ctx.closePath()
@@ -36,3 +63,4 @@ chart(document.getElementById('chart'),
         [1000,50],
     ]
 )
+
